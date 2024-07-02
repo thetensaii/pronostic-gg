f20146e4c6732602125b0ec8bd3c6e025b8248da
TheTensaii <f.salimou@hotmail.fr>
2024-07-02 04:21:39 -0400

    fix(front): put table row in a body

--- apps/front/src/leaderboard/ui/molecule/CurrentMemberRow.tsx
+++ apps/front/src/leaderboard/ui/molecule/CurrentMemberRow.tsx
@@ -9,11 +9,13 @@ type Props = {
 export const CurrentMemberRow = ({ member } : Props) => {
   return (
     <Table.Root variant='outline' css={{ w: 'full'}}>
-      <Table.Row>
-          <Table.Cell css={{ fontWeight: 'medium' }}><Text as="span" size='lg'>{member.rank}</Text></Table.Cell>
-          <Table.Cell css={{ fontWeight: 'medium' }}><Text as="span" size='lg'>{member.username}</Text></Table.Cell>
-          <Table.Cell css={{ fontWeight: 'medium' }}><Text as="span" size='lg'>{member.points}</Text></Table.Cell>
-      </Table.Row>
+      <Table.Body>
+        <Table.Row>
+            <Table.Cell css={{ fontWeight: 'medium' }}><Text as="span" size='lg'>{member.rank}</Text></Table.Cell>
+            <Table.Cell css={{ fontWeight: 'medium' }}><Text as="span" size='lg'>{member.username}</Text></Table.Cell>
+            <Table.Cell css={{ fontWeight: 'medium' }}><Text as="span" size='lg'>{member.points}</Text></Table.Cell>
+        </Table.Row>
+      </Table.Body>
     </Table.Root>
   )
 }
