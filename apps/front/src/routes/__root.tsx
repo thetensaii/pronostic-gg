e993eda5683f289306d7070e758dc8d12b3092e3
TheTensaii <f.salimou@hotmail.fr>
2024-07-06 15:25:53 -0400

    feat: handle default not found

--- apps/front/src/routes/__root.tsx
+++ apps/front/src/routes/__root.tsx
@@ -1,7 +1,21 @@
-import { createRootRoute, Outlet } from '@tanstack/react-router'
+import { createRootRoute, Link as RouterLink, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/router-devtools'
+import { Container } from 'styled-system/jsx'
+import { Link } from '~/components/ui/link'
+import { Text } from '~/components/ui/text'
 
 export const Route = createRootRoute({
+  notFoundComponent: () => {
+    return (
+      <Container css={{ maxWidth: '3xl',  }}>
+        <Text>Erreur cette page n'existe pas.</Text>
+        
+        <Link asChild>
+          <RouterLink to="/">Revenir sur le site</RouterLink>
+        </Link>
+      </Container>
+    )
+  },
   component: () => (
     <>
       <Outlet />
