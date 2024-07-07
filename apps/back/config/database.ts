d70a97c1eb33a39063b2e9e7fe33fc35d1ba5f13
TheTensaii <f.salimou@hotmail.fr>
2024-07-07 09:30:27 -0400

    chore: do not allow rollback in prod

--- apps/back/config/database.ts
+++ apps/back/config/database.ts
@@ -16,6 +16,7 @@ const dbConfig = defineConfig({
       migrations: {
         naturalSort: true,
         paths: ['database/migrations'],
+        disableRollbacksInProduction: true,
       },
     },
   },
